8345fd9dd5bad5fd697cd5d631e3ada0f9051e50
Yudha13Koley <yudhajit1998@gmail.com>
2020-10-03 16:03:03 +0530

    UC2 With all Test Cases Done

--- src/main/java/com/training/FindMaximum.java
+++ src/main/java/com/training/FindMaximum.java
@@ -1,8 +1,16 @@
 package com.training;
 
 public class FindMaximum {
-	public static Integer findMax(Integer a,Integer b,Integer c) {
+	public static Integer findMaxInt(Integer a,Integer b,Integer c) {
 		Integer max=a;
+		if(b.compareTo(max)>0)
+			max=b;
+		if(c.compareTo(max)>0)
+			max=c;
+		return max;
+	}
+	public static Float findMaxFloat(Float a,Float b,Float c) {
+		Float max=a;
 		if(b.compareTo(max)>0)
 			max=b;
 		if(c.compareTo(max)>0)
--- src/test/java/com/training/TestMaximum.java
+++ src/test/java/com/training/TestMaximum.java
@@ -8,19 +8,34 @@ import org.junit.Test;
 public class TestMaximum {
 
 	@Test
-	public void givenMaxValueAtFirst_ReturnsMaxValue() {
-		Integer a=FindMaximum.findMax(10,5,2);
+	public void givenMaxIntValueAtFirst_ReturnsMaxValue() {
+		Integer a=FindMaximum.findMaxInt(10,5,2);
 		assertEquals(10, a.intValue());
 	}
 	@Test
-	public void givenMaxValueAtSecond_ReturnsMaxValue() {
-		Integer a=FindMaximum.findMax(5,7,2);
+	public void givenMaxIntValueAtSecond_ReturnsMaxValue() {
+		Integer a=FindMaximum.findMaxInt(5,7,2);
 		assertEquals(7, a.intValue());
 	}
 	@Test
-	public void givenMaxValueAtThird_ReturnsMaxValue() {
-		Integer a=FindMaximum.findMax(-7,-5,-2);
+	public void givenMaxIntValueAtThird_ReturnsMaxValue() {
+		Integer a=FindMaximum.findMaxInt(-7,-5,-2);
 		assertEquals(-2, a.intValue());
+	}
+	@Test
+	public void givenMaxFloatValueAtFirst_ReturnsMaxValue() {
+		Float a=FindMaximum.findMaxFloat(10.08f,10.01f,10.00f);
+		assertEquals(10.08f,a.floatValue(),0.01f);
+	}
+	@Test
+	public void givenMaxFloatValueAtSecond_ReturnsMaxValue() {
+		Float a=FindMaximum.findMaxFloat(5.2552f,5.343f,5.234f);
+		assertEquals(5.343f, a.floatValue(),0.001f);
+	}
+	@Test
+	public void givenMaxFloatValueAtThird_ReturnsMaxValue() {
+		Float a=FindMaximum.findMaxFloat(-1.71f,-1.21f,-1.11f);
+		assertEquals(-1.11f, a.floatValue(),0.01f);
 	}
 
 }
