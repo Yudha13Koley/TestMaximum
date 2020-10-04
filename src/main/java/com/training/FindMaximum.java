06452eba3120f2789b5d13a3e6f94f975f428e2a
Yudha13Koley <yudhajit1998@gmail.com>
2020-10-04 09:17:49 +0530

    Refactor-2 Done

--- src/main/java/com/training/FindMaximum.java
+++ src/main/java/com/training/FindMaximum.java
@@ -1,8 +1,16 @@
 package com.training;
 
-public class FindMaximum {
-	public static <E extends Comparable<E>> E findMax(E a,E b,E c) {
-		E max=a;
+public class FindMaximum<E extends Comparable<E>>{
+	public E a;
+	public E b;
+	public E c;
+	public FindMaximum(E a,E b,E c) {
+		this.a=a;
+		this.b=b;
+		this.c=c;
+	}
+	public E findMax(){
+	E max=a;
 		if(b.compareTo(max)>0)
 			max=b;
 		if(c.compareTo(max)>0)
--- src/test/java/com/training/TestMaximum.java
+++ src/test/java/com/training/TestMaximum.java
@@ -9,47 +9,47 @@ public class TestMaximum {
 
 	@Test
 	public void givenMaxIntValueAtFirst_ReturnsMaxValue() {
-		Integer a=FindMaximum.<Integer>findMax(10,5,2);
+		Integer a=new FindMaximum<Integer>(10,5,2).findMax();
 		assertEquals(10, a.intValue());
 	}
 	@Test
 	public void givenMaxIntValueAtSecond_ReturnsMaxValue() {
-		Integer a=FindMaximum.<Integer>findMax(5,7,2);
-		assertEquals(7, a.intValue());
+		Integer a=new FindMaximum<Integer>(2,6,5).findMax();
+		assertEquals(6, a.intValue());
 	}
 	@Test
 	public void givenMaxIntValueAtThird_ReturnsMaxValue() {
-		Integer a=FindMaximum.<Integer>findMax(-7,-5,-2);
+		Integer a=new FindMaximum<Integer>(-3,-6,-2).findMax();
 		assertEquals(-2, a.intValue());
 	}
 	@Test
 	public void givenMaxFloatValueAtFirst_ReturnsMaxValue() {
-		Float a=FindMaximum.<Float>findMax(10.08f,10.01f,10.00f);
+		Float a=new FindMaximum<Float>(10.08f,10.01f,10.00f).findMax();
 		assertEquals(10.08f,a.floatValue(),0.01f);
 	}
 	@Test
 	public void givenMaxFloatValueAtSecond_ReturnsMaxValue() {
-		Float a=FindMaximum.<Float>findMax(5.2552f,5.343f,5.234f);
+		Float a=new FindMaximum<Float>(5.2552f,5.343f,5.234f).findMax();
 		assertEquals(5.343f, a.floatValue(),0.001f);
 	}
 	@Test
 	public void givenMaxFloatValueAtThird_ReturnsMaxValue() {
-		Float a=FindMaximum.<Float>findMax(-1.71f,-1.21f,-1.11f);
+		Float a=new FindMaximum<Float>(-1.71f,-1.21f,-1.11f).findMax();
 		assertEquals(-1.11f, a.floatValue(),0.01f);
 	}
 	@Test
 	public void givenMaxStringValueAtFirst_ReturnsMaxValue() {
-		String a=FindMaximum.<String>findMax("peach","apple", "banana");
+		String a=new FindMaximum<String>("peach","apple", "banana").findMax();
 		assertEquals("peach",a);
 	}
 	@Test
 	public void givenMaxStringValueAtSecond_ReturnsMaxValue() {
-		String a=FindMaximum.<String>findMax("apple","peach","banana");
+		String a= new FindMaximum<String>("apple","peach","banana").findMax();
 		assertEquals("peach",a);
 	}
 	@Test
 	public void givenMaxStringValueAtThird_ReturnsMaxValue() {
-		String a=FindMaximum.<String>findMax("apple","banana","peach");
+		String a=new FindMaximum<String>("apple","banana","peach").findMax();
 		assertEquals("peach",a);
 		}
 
